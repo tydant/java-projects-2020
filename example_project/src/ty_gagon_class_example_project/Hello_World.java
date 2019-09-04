2fcee0f5a279ec7e74e61bcd2f5881d712e56b52
Ty Gagon <ty.gagon@tooeleschools.org>
2019-09-04 14:11:05 -0600

    chapter 3 names langueges

---
+++ example_project/src/ty_gagon_class_example_project/Hello_World.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ty_gagon_class_example_project;
+
+/**
+ * @author ty.gagon
+ *hello world
+ * 9/19
+ *
+ */
+public class Hello_World {
+
+	/**
+	 * 
+	 */
+	public Hello_World() {
+		System.out.println("hello world");
+		System.out.println("Im sick of this");
+	    String hello = "hello";
+	    System.out.println(hello);
+	}
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	new Hello_World();
+
+	}
+
+}
---
+++ example_project/src/ty_gagon_name_classes_example_project/Name_and_Classes.java
@@ -0,0 +1,22 @@
+package ty_gagon_name_classes_example_project;
+
+import ty_gagon_class_example_project.Hello_World;
+
+public class Name_and_Classes {
+
+	public Name_and_Classes() {
+		System.out.println("Ty Gagon");
+		System.out.println("period 1 11 Grade Math");
+		System.out.println("period 2 psychology");
+		System.out.println("period 3 U.S History");
+		System.out.println("period 4 11 Grade Language Arts");
+		System.out.println("period 5 11 Chemistry");
+		System.out.println("period 6&7 11 A+ Comp. Repair");
+		System.out.println("period 8 11 Foods and Nutition");
+		System.out.println("period 9&10 Java");
+	}
+	public static void main(String[] args) {
+	new Name_and_Classes();
+	}
+
+}
