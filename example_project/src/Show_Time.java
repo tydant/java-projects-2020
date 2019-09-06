be3e289916caa56dd70d92236f1ec02fb14ca512
Ty Gagon <ty.gagon@tooeleschools.org>
2019-09-06 11:43:06 -0600

    eclipse show time

---
+++ example_project/src/Show_Time.java
@@ -0,0 +1,13 @@
+
+public class Show_Time {
+
+	public Show_Time() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ example_project/src/ty_gagon_name_classes_example_project/Show_Time.java
@@ -0,0 +1,16 @@
+package ty_gagon_name_classes_example_project;
+import java.util.Calendar;
+
+public class Show_Time {
+
+	public Show_Time() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Calendar.getInstance().getTime());
+
+	}
+
+}
